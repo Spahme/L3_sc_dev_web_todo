aeb305c29086786fe872e896a893c15eee32ad52
daza sasha <dazasasha@MacBookProSD.local>
2025-10-06 09:10:30 +0200

    Update

--- src/components/Task/Update.tsx
+++ src/components/Task/Update.tsx
@@ -2,12 +2,12 @@ import type { Task } from "../../Task";
 
 export function Update(
   id: string,
-  AddNewTask: React.Dispatch<React.SetStateAction<Task[]>>
+  UpdateTask: React.Dispatch<React.SetStateAction<Task[]>>
 ) {
   const newContent = prompt("Entrez le nouveau contenu de la tâche :");
   if (!newContent) return;
 
-  AddNewTask(prev =>
+  UpdateTask(prev =>
     prev.map(task =>
       task.id === id ? { ...task, content: newContent.trim(), updatedAt: new Date(), status: "todo" } : task
     )
